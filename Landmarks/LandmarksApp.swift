71b1cf5d800e018ddf5a3e4146525f4e9ae80152
Sophia <44929270+slitechs@users.noreply.github.com>
2023-06-05 11:46:31 -0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Sophia Li on 2023-06-05.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
