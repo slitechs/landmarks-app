a67a6af043af268256d120c0e918a8f3cc958c81
Sophia <44929270+slitechs@users.noreply.github.com>
2023-06-06 13:01:52 -0700

    Update LandmarkList.swift

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -25,8 +25,10 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
-            .previewDevice(PreviewDevice(rawValue: "iPhone SE (3rd generation)"))
-
+        ForEach(["iPhone SE (3rd generation)", "iPhone 14"], id: \.self) { deviceName in
+                    LandmarkList()
+                        .previewDevice(PreviewDevice(rawValue: deviceName))
+                        .previewDisplayName(deviceName)
+                }
     }
 }
